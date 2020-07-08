12e78f308a9584865f8e0866925cfdaa237ee886
jlam609 <jlam609@gmail.com>
2020-07-08 19:10:35 -0400

    finished

--- actions.js
+++ actions.js
@@ -12,22 +12,19 @@ const gotDesserts = (desserts) => ({
     type:NEW_DESSERT_DATA,
     newDessert,
  })
-const fetchDesserts = async() => {
-    const desserts = (await axios.get('/api/desserts')).data
-    return ({
-        type:GOT_DESSERTS_DATA,
-        desserts
-    })
+const fetchDesserts = () => {
+    return async (dispatch) => {
+        const desserts = (await axios.get('/api/desserts')).data.desserts
+        return dispatch(gotDesserts(desserts))
+    }
 }
 
 
-const postDessert = async() => {
-    await axios.post('/api/desserts')
-    const desserts = (await axios.get('/api/desserts')).data
-    return ({
-        type:GOT_DESSERTS_DATA,
-        desserts
-})
+const postDessert = (dessertObj) => {
+    return async (dispatch) => {
+        const dessert = (await axios.post('/api/desserts', dessertObj)).data.newDessert
+        return dispatch(addDessert(dessert))
+    }
 }
 
 module.exports = { gotDesserts, addDessert, fetchDesserts, postDessert };
